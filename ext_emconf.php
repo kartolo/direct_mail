5a1aa0e3ef2ca2c59f45e691447bf48aa53e3c59
revsbech <revsbech@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-06-03 08:38:00 +0000

    After uploading to TER, the version humber was chagend from 1.0.8 to 1.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail/trunk@384 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: 'direct_mail'
+# Extension Manager/Repository config file for ext: "direct_mail"
 # 
-# Auto generated 12-02-2003 21:24
+# Auto generated 03-06-2005 10:35
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
 ########################################################################
-
-/**
- *
- * @package TYPO3
- * @subpackage tx_directmail
- * @version $Id$
- */
 
 $EM_CONF[$_EXTKEY] = Array (
 	'title' => 'Web>Plugins, Direct Mail',
@@ -24,7 +17,9 @@ $EM_CONF[$_EXTKEY] = Array (
 	'dependencies' => 'cms,tt_address,plugin_mgm',
 	'conflicts' => '',
 	'priority' => '',
-	'lockType' => 'G',
+	'loadOrder' => '',
+	'TYPO3_version' => '-',
+	'PHP_version' => '-',
 	'module' => '',
 	'state' => 'beta',
 	'internal' => 0,
@@ -32,13 +27,16 @@ $EM_CONF[$_EXTKEY] = Array (
 	'createDirs' => '',
 	'modify_tables' => 'tt_content,tt_address,fe_users',
 	'clearCacheOnLoad' => 0,
+	'lockType' => 'G',
 	'author' => 'Kasper Skårhøj',
 	'author_email' => 'kasper@typo3.com',
 	'author_company' => 'Curby Soft Multimedia',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '1.0.8',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:11:{s:12:"ext_icon.gif";s:4:"a143";s:14:"ext_tables.php";s:4:"7234";s:14:"ext_tables.sql";s:4:"5778";s:17:"locallang_tca.php";s:4:"9a9b";s:14:"mod/attach.gif";s:4:"5559";s:24:"mod/class.mailselect.php";s:4:"3aca";s:27:"mod/class.mod_web_dmail.php";s:4:"5a1c";s:20:"mod/dmailerd.phpcron";s:4:"ef4c";s:17:"mod/locallang.php";s:4:"38f5";s:12:"mod/mail.gif";s:4:"34a5";s:20:"mod/returnmail.phpsh";s:4:"ef25";}',
+	'version' => '1.1.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"47c0";s:12:"ext_icon.gif";s:4:"a143";s:14:"ext_tables.php";s:4:"ac00";s:14:"ext_tables.sql";s:4:"b679";s:15:"ext_tables.sql~";s:4:"41c9";s:26:"locallang_csh_sysdmail.php";s:4:"b21d";s:27:"locallang_csh_sysdmailg.php";s:4:"aa99";s:17:"locallang_tca.php";s:4:"ea68";s:12:"doc/TODO.txt";s:4:"2114";s:14:"mod/attach.gif";s:4:"5559";s:24:"mod/class.mailselect.php";s:4:"f23e";s:27:"mod/class.mod_web_dmail.php";s:4:"0965";s:20:"mod/dmailerd.phpcron";s:4:"c5c9";s:21:"mod/dmailerd.phpcron~";s:4:"c5c9";s:17:"mod/locallang.php";s:4:"752b";s:12:"mod/mail.gif";s:4:"34a5";s:20:"mod/returnmail.phpsh";s:4:"cd64";}',
 );
 
 ?>
