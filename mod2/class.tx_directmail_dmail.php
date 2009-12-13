0e269db4cf81babe94cdfa8085c99dae6ad96aac
ivankartolo <ivankartolo@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-13 16:18:07 +0000

    * bug #5698(f): fixed link to edit page
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/direct_mail/trunk@27651 735d13b6-9817-0410-8766-e36946ffe9aa

--- mod2/class.tx_directmail_dmail.php
+++ mod2/class.tx_directmail_dmail.php
@@ -1750,7 +1750,7 @@ class tx_directmail_dmail extends t3lib_SCbase {
 				$outLines[] = array(
 					'<a href="'.$createDmailLink.'">'.t3lib_iconWorks::getIconImage('pages', $row, $BACK_PATH, ' title="'.htmlspecialchars(t3lib_BEfunc::getRecordPath ($row['uid'],$this->perms_clause,20)).'" style="vertical-align: top;"').htmlspecialchars($row['title']).'</a>',
 					'<a href="'.$createDmailLink.'"><img'.t3lib_iconWorks::skinImg($BACK_PATH, 'gfx/newmail', 'width="16" height="18"').' alt="'.$LANG->getLL("dmail_createMail").'" style="vertical-align:top;" title="'.$LANG->getLL("nl_create").'" /></a>',
-					'<a href="/typo3/'.t3lib_extMgm::extRelPath('cms').'layout/db_layout.php?id='.$row['uid'].'" target="_blank"><img'.t3lib_iconWorks::skinImg($BACK_PATH, 'gfx/edit2.gif', 'width="12" height="12"').' alt="'.$LANG->getLL("dmail_edit").'" style="vertical-align:top;" title="'.$LANG->getLL("nl_editPage").'" /></a>',
+					'<a href="'.PATH_typo3.t3lib_extMgm::extRelPath('cms').'layout/db_layout.php?id='.$row['uid'].'" target="_blank"><img'.t3lib_iconWorks::skinImg($BACK_PATH, 'gfx/edit2.gif', 'width="12" height="12"').' alt="'.$LANG->getLL("dmail_edit").'" style="vertical-align:top;" title="'.$LANG->getLL("nl_editPage").'" /></a>',
 					$iconPreview
 					);
 			}
