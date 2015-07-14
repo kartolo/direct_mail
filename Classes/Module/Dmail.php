d7d45c301dfb0a517e2d87072a8329b1d015f554
Ivan Kartolo <ivan.kartolo@gmail.com>
2015-07-14 16:21:02 +0200

    [BUGFIX]  regression: syntax error
    fix regression. PHP syntax error is fixed
    
    Resolves: #62625
    Releases: 4.0
    Change-Id: Iabc08a6366cc41e888d128f45c095c1d4cbbe066

--- Classes/Module/Dmail.php
+++ Classes/Module/Dmail.php
@@ -1005,7 +1005,7 @@ class Dmail extends \TYPO3\CMS\Backend\Module\BaseScriptClass {
 				}
 
 				$updateFields = array(
-					'recipientGroups' => implode(',', $recipientGroups);
+					'recipientGroups' => implode(',', $recipientGroups),
 					'scheduled'  => $distributionTime,
 					'query_info' => serialize($queryInfo)
 				);
