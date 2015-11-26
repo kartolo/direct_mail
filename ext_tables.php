454cf5cae7c824e4b3b56ddd2c64883cebdd5e3c
Ivan Kartolo <ivan.kartolo@gmail.com>
2015-11-26 21:19:18 +0100

    [BUGFIX] remove use namespace in global file
    remove use statements in ext_tables.php, since
    TYPO3 concatenate this with other extensions
    
    Resolves: #71870
    Releases: 5.0
    Change-Id: I91065ac0652227032f6df85c07e8016c85efea95
    Reviewed-on: https://review.typo3.org/44965
    Reviewed-by: Ivan Dharma Kartolo <ivan.kartolo@gmail.com>
    Tested-by: Ivan Dharma Kartolo <ivan.kartolo@gmail.com>

--- ext_tables.php
+++ ext_tables.php
@@ -1,21 +1,17 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use \TYPO3\CMS\Backend\Sprite\SpriteManager;
-
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$extPath = ExtensionManagementUtility::extPath($_EXTKEY);
+$extPath = TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY);
 
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/boundaries/','Direct Mail Content Boundaries');
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/plaintext/', 'Direct Mail Plain text');
-ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/tt_news_plaintext/', 'Direct Mail News Plain text');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/boundaries/','Direct Mail Content Boundaries');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/plaintext/', 'Direct Mail Plain text');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/tt_news_plaintext/', 'Direct Mail News Plain text');
 
 	// Category field disabled by default in backend forms.
-ExtensionManagementUtility::addPageTSConfig('
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
 	TCEFORM.tt_content.module_sys_dmail_category.disabled = 1
 	TCEFORM.tt_address.module_sys_dmail_category.disabled = 1
 	TCEFORM.fe_users.module_sys_dmail_category.disabled = 1
@@ -25,16 +21,16 @@ ExtensionManagementUtility::addPageTSConfig('
 /**
  * Setting up the direct mail module
  */
-ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_sysdmail.xml');
-ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail_group','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_sysdmailg.xml');
-ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail_category','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_sysdmailcat.xml');
-ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM2','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM2.xml');
-ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM3','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM3.xml');
-ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM4','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM4.xml');
-ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM5','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM5.xml');
-ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM6','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM6.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_sysdmail.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail_group','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_sysdmailg.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail_category','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_sysdmailcat.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM2','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM2.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM3','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM3.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM4','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM4.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM5','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM5.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_txdirectmailM1_txdirectmailM6','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_txdirectmailM6.xml');
 //old
-ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_web_txdirectmailM1','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_web_txdirectmail.xml');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_web_txdirectmailM1','EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_web_txdirectmail.xml');
 
 
 if (TYPO3_MODE == 'BE') {
@@ -53,7 +49,7 @@ if (TYPO3_MODE == 'BE') {
 		$TBE_MODULES = $temp_TBE_MODULES;
 	}
 
-	ExtensionManagementUtility::addModule('txdirectmailM1', '', '', $extPath . 'mod1/',
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('txdirectmailM1', '', '', $extPath . 'mod1/',
 		array(
 			'script' => '_DISPATCH',
 			'access' => 'group,user',
@@ -67,7 +63,7 @@ if (TYPO3_MODE == 'BE') {
 		)
 	);
 
-	ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM2', 'bottom', $extPath . 'mod2/',
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM2', 'bottom', $extPath . 'mod2/',
 		array(
 			'script' => '_DISPATCH',
 			'access' => 'group,user',
@@ -84,7 +80,7 @@ if (TYPO3_MODE == 'BE') {
 		)
 	);
 
-	ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM3', 'bottom', $extPath . 'mod3/',
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM3', 'bottom', $extPath . 'mod3/',
 		array(
 			'script' => '_DISPATCH',
 			'access' => 'group,user',
@@ -101,7 +97,7 @@ if (TYPO3_MODE == 'BE') {
 		)
 	);
 
-	ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM4', 'bottom', $extPath . 'mod4/',
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM4', 'bottom', $extPath . 'mod4/',
 		array(
 			'script' => '_DISPATCH',
 			'access' => 'group,user',
@@ -118,7 +114,7 @@ if (TYPO3_MODE == 'BE') {
 		)
 	);
 
-	ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM5', 'bottom', $extPath . 'mod5/',
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM5', 'bottom', $extPath . 'mod5/',
 		array(
 			'script' => '_DISPATCH',
 			'access' => 'group,user',
@@ -136,7 +132,7 @@ if (TYPO3_MODE == 'BE') {
 	);
 
 
-	ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM6', 'bottom', $extPath . 'mod6/',
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule('txdirectmailM1', 'txdirectmailM6', 'bottom', $extPath . 'mod6/',
 		array(
 			'script' => '_DISPATCH',
 			'access' => 'group,user',
@@ -154,12 +150,12 @@ if (TYPO3_MODE == 'BE') {
 	);
 }
 
-SpriteManager::addTcaTypeIcon(
+TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
 	'pages',
 	'contains-dmail',
-	ExtensionManagementUtility::extRelPath($_EXTKEY) . 'res/gfx/ext_icon_dmail_folder.gif'
+	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'res/gfx/ext_icon_dmail_folder.gif'
 );
 
-if (VersionNumberUtility::convertVersionNumberToInteger(ExtensionManagementUtility::getExtensionVersion('tt_address')) <= VersionNumberUtility::convertVersionNumberToInteger('2.3.5')) {
-	include_once(ExtensionManagementUtility::extPath($_EXTKEY)."Configuration/TCA/Overrides/tt_address.php");
+if (TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getExtensionVersion('tt_address')) <= VersionNumberUtility::convertVersionNumberToInteger('2.3.5')) {
+	include_once(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY)."Configuration/TCA/Overrides/tt_address.php");
 }
