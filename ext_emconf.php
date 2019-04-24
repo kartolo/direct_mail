05dfe489ff30e269f858d2099ccb9966279cd8ab
Florian Wessels <f.wessels@bitmotion.de>
2019-04-24 15:07:20 +0200

    [TASK] Restore TYPO3 8 LTS compatibility in composer and ext_emconf file

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'tt_address' => '',
             'php' => '7.2.0',
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '8.7.0-9.5.99',
             'rdct' => '1.0.0',
         ),
         'conflicts' => array(
