e7045dc0fc5a52d577bd16af8078558e451d2ff4
Goran Tusek <goran.tusek@na-media.de>
2019-07-15 10:38:09 +0200

    #164 UseImplicitPortToFetch documentation

--- ext_localconf.php
+++ ext_localconf.php
@@ -82,7 +82,7 @@ $TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['cronInt'] = $extConf['cronInt'];
 $TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['UseHttpToFetch'] = $extConf['UseHttpToFetch'];
 
 /**
- * Do NOT use PORT from which your are accessing your TYPO3 Backend to fetch contents from one of your Frontends, but use implicit port instead (e.g. no explicit port in URL)
+ * Use implicit port in URL for fetching Newsletter-Content: Even if your TYPO3 Backend is on a non-standard-port, the URL for fetching the newsletter contents from one of your Frontend-Domains will not use the PORT you are using to access your TYPO3 Backend, but use implicit port instead (e.g. no explicit port in URL)
  */
 $TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['UseImplicitPortToFetch'] = $extConf['UseImplicitPortToFetch'];
 
