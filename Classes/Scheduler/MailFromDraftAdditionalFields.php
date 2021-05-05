16bc876694c5dca8c493c0a73d6144bc69f4ab57
Ivan Kartolo <ivan.kartolo@gmail.com>
2021-05-05 21:05:59 +0200

    [BUGFIX] fix deprecated addMessage in the scheduler additionalField
    Resolves: #253
    Releases: develop

--- Classes/Scheduler/MailFromDraftAdditionalFields.php
+++ Classes/Scheduler/MailFromDraftAdditionalFields.php
@@ -15,9 +15,12 @@ namespace DirectMailTeam\DirectMail\Scheduler;
  */
 
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
-use TYPO3\CMS\Scheduler\AdditionalFieldProviderInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Scheduler\AbstractAdditionalFieldProvider;
 use TYPO3\CMS\Scheduler\Controller\SchedulerModuleController;
+use TYPO3\CMS\Scheduler\Task\AbstractTask;
 
 /**
  * Aditional fields provider class for usage with the Scheduler's MailFromDraft task
@@ -26,7 +29,7 @@ use TYPO3\CMS\Scheduler\Controller\SchedulerModuleController;
  * @package		TYPO3
  * @subpackage	direct_mail
  */
-class MailFromDraftAdditionalFields implements AdditionalFieldProviderInterface
+class MailFromDraftAdditionalFields extends AbstractAdditionalFieldProvider
 {
 
     /**
@@ -35,7 +38,7 @@ class MailFromDraftAdditionalFields implements AdditionalFieldProviderInterface
      *
      * @param	array					$taskInfo reference to the array containing the info used in the add/edit form
      * @param	object					$task when editing, reference to the current task object. Null when adding.
-     * @param	\TYPO3\CMS\Scheduler\Controller\SchedulerModuleController		$parentObject reference to the calling object (Scheduler's BE module)
+     * @param	SchedulerModuleController		$schedulerModuleController reference to the calling object (Scheduler's BE module)
      *
      * @return	array					Array containg all the information pertaining to the additional fields
      *									The array is multidimensional, keyed to the task class name and each field's id
@@ -45,12 +48,11 @@ class MailFromDraftAdditionalFields implements AdditionalFieldProviderInterface
      *										['cshKey']		=> The CSH key for the field
      *										['cshLabel']	=> The code of the CSH label
      */
-    public function getAdditionalFields(array &$taskInfo, $task, SchedulerModuleController $parentObject)
+    public function getAdditionalFields(array &$taskInfo, $task, SchedulerModuleController $schedulerModuleController)
     {
-
-            // Initialize extra field value
+        // Initialize extra field value
         if (empty($taskInfo['selecteddraft'])) {
-            if ($parentObject->CMD == 'edit') {
+            if ($schedulerModuleController->CMD === 'edit') {
                 // In case of edit, and editing a test task, set to internal value if not data was submitted already
                 $taskInfo['selecteddraft'] = $task->draftUid;
             } else {
@@ -62,7 +64,7 @@ class MailFromDraftAdditionalFields implements AdditionalFieldProviderInterface
         // fetch all available drafts
         $drafts = array();
 
-        $queryBuilder =  \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class)
+        $queryBuilder =  GeneralUtility::makeInstance(ConnectionPool::class)
             ->getQueryBuilderForTable('sys_dmail');
         $draftsInternal = $queryBuilder
             ->select('*')
@@ -99,12 +101,11 @@ class MailFromDraftAdditionalFields implements AdditionalFieldProviderInterface
         } else {
             foreach ($drafts as $draft) {
                 // see #44577
-                $selected = ($task->draftUid == $draft['uid'] ? ' selected="selected"' : '');
+                $selected = ($task->draftUid === $draft['uid'] ? ' selected="selected"' : '');
                 $fieldHtml .= '<option value="' . $draft['uid'] . '"' . $selected . '>' . $draft['subject'] . ' [' . $draft['uid'] . ']</option>';
             }
         }
         $fieldHtml = '<select name="tx_scheduler[selecteddraft]" id="' . $fieldID . '">' . $fieldHtml . '</select>';
-
 
         $additionalFields = array();
         $additionalFields[$fieldID] = array(
@@ -124,17 +125,17 @@ class MailFromDraftAdditionalFields implements AdditionalFieldProviderInterface
      * If the task class is not relevant, the method is expected to return true
      *
      * @param	array					$submittedData Reference to the array containing the data submitted by the user
-     * @param	\TYPO3\CMS\Scheduler\Controller\SchedulerModuleController		$parentObject Reference to the calling object (Scheduler's BE module)
+     * @param	SchedulerModuleController		$schedulerModuleController Reference to the calling object (Scheduler's BE module)
      *
      * @return	bool					True if validation was ok (or selected class is not relevant), false otherwise
      */
-    public function validateAdditionalFields(array &$submittedData, \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $parentObject)
+    public function validateAdditionalFields(array &$submittedData, SchedulerModuleController $schedulerModuleController)
     {
-        $draftUid = $submittedData['selecteddraft'] = intval($submittedData['selecteddraft']);
+        $draftUid = $submittedData['selecteddraft'] = (int) $submittedData['selecteddraft'];
         if ($draftUid > 0) {
             $draftRecord = BackendUtility::getRecord('sys_dmail', $draftUid);
 
-            $queryBuilder =  \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class)
+            $queryBuilder =  GeneralUtility::makeInstance(ConnectionPool::class)
                 ->getQueryBuilderForTable('sys_dmail');
             $draftsInternal = $queryBuilder
                 ->select('*')
@@ -145,19 +146,16 @@ class MailFromDraftAdditionalFields implements AdditionalFieldProviderInterface
                 ->execute()
                 ->fetchAll();
 
-
-
-
             if ($draftRecord['type'] == 2 || $draftRecord['type'] == 3) {
                 $result = true;
             } else {
                 // TODO: localization
-                $parentObject->addMessage('No draft record selected', FlashMessage::ERROR);
+                $this->addMessage('No draft record selected', FlashMessage::ERROR);
                 $result = false;
             }
         } else {
             // TODO: localization
-            $parentObject->addMessage('No drafts found. Please add one first through the direct mail process', FlashMessage::ERROR);
+            $this->addMessage('No drafts found. Please add one first through the direct mail process', FlashMessage::ERROR);
             $result = false;
         }
 
@@ -169,11 +167,11 @@ class MailFromDraftAdditionalFields implements AdditionalFieldProviderInterface
      * if the task class matches
      *
      * @param	array				$submittedData Array containing the data submitted by the user
-     * @param	\TYPO3\CMS\Scheduler\Task\AbstractTask	$task Reference to the current task object
+     * @param	AbstractTask	$task Reference to the current task object
      *
      * @return	void
      */
-    public function saveAdditionalFields(array $submittedData, \TYPO3\CMS\Scheduler\Task\AbstractTask $task)
+    public function saveAdditionalFields(array $submittedData, AbstractTask $task)
     {
         $task->setDraft($submittedData['selecteddraft']);
     }
