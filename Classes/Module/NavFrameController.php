3ea6784d2b3fa53d346d5d60236956e94c36cead
Igor Neyman <igorneyman82@gmail.com>
2021-10-31 19:53:50 +0000

    add crowdin.yml

--- Classes/Module/NavFrameController.php
+++ Classes/Module/NavFrameController.php
@@ -128,11 +128,11 @@ class NavFrameController
             
             // Call this function, refresh_nav(), from another script in the backend if you want to refresh the navigation frame (eg. after having changed a page title or moved pages etc.)
 			// See t3lib_BEfunc::getSetUpdateSignal()
-			function refresh_nav() { //
+			function refresh_nav() {
 				window.setTimeout("_refresh_nav();",0);
 			}
             
-			function _refresh_nav()	{ //
+			function _refresh_nav()	{
 				document.location="' . htmlspecialchars(GeneralUtility::getIndpEnv('SCRIPT_NAME') . '?unique=' . time()) . '";
 			}
             
