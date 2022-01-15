6e11c4f0d639a5e280d2f04b59f8c46f72569756
Igor Neyman <igorneyman82@gmail.com>
2022-01-15 13:11:48 +0000

    update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,26 +4,21 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Direct Mail',
     'description' => 'Advanced Direct Mail/Newsletter mailer system with sophisticated options for personalization of emails including response statistics.',
     'category' => 'module',
-    'version' => '7.0.1',
-    'state' => 'stable',
-    'clearcacheonload' => 0,
-    'lockType' => '',
     'author' => 'Ivan Kartolo',
     'author_email' => 'ivan.kartolo@dkd.de',
     'author_company' => 'd.k.d Internet Service GmbH',
+    'state' => 'stable',
+    'clearcacheonload' => 0,
+    'version' => '8.0.0',
     'constraints' => [
         'depends' => [
-            'tt_address' => '4.0.0-',
-            'php' => '7.2.0',
-            'typo3' => '11.5.0-11.9.99',
-            'jumpurl' => '8.0.0-',
+            'typo3' => '11.5.0-11.99.99',
+            'tt_address' => '5.3.0-',
+            'php' => '7.4.0-8.0.99',
+            'jumpurl' => '8.0.3-',
             'rdct' => '2.0.0'
         ],
         'conflicts' => [
-            'sr_direct_mail_ext' => '',
-            'it_dmail_fix' => '',
-            'plugin_mgm' => '',
-            'direct_mail_123' => '',
         ],
         'suggests' => [
         ],
