c51d1217758e497b88a1c3106c0b59d35cb795dc
Igor Neyman <in@points.de>
2022-08-07 21:46:25 +0200

    update ext_localconf.php and ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,31 +1,31 @@
 <?php
+declare(strict_types=1);
 
-defined('TYPO3_MODE') or die();
+defined('TYPO3') || die();
 
-
+// https://docs.typo3.org/m/typo3/reference-coreapi/11.5/en-us/ExtensionArchitecture/BestPractises/ConfigurationFiles.html
+(function () {
     // Category field disabled by default in backend forms.
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-	TCEFORM.tt_content.module_sys_dmail_category.disabled = 1
-	TCEFORM.tt_address.module_sys_dmail_category.disabled = 1
-	TCEFORM.fe_users.module_sys_dmail_category.disabled = 1
-	TCEFORM.sys_dmail_group.select_categories.disabled = 1
-');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+    	TCEFORM.tt_content.module_sys_dmail_category.disabled = 1
+    	TCEFORM.tt_address.module_sys_dmail_category.disabled = 1
+    	TCEFORM.fe_users.module_sys_dmail_category.disabled = 1
+    	TCEFORM.sys_dmail_group.select_categories.disabled = 1
+    ');
 
-/**
- * Setting up the direct mail module
- */
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_sysdmail.xlf');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail_group', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_sysdmailg.xlf');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail_category', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_sysdmailcat.xlf');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_DirectMail', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_DirectMail.xlf');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_RecipientList', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_RecipientList.xlf');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_Statistics', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_Statistics.xlf');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_MailerEngine', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_MailerEngine.xlf');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_Configuration', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_Configuration.xlf');
-//old
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_web_DirectMailNavFrame', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_web_txdirectmail.xlf');
-
-if (TYPO3_MODE == 'BE') {
+    /**
+     * Setting up the direct mail module
+     */
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_sysdmail.xlf');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail_group', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_sysdmailg.xlf');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('sys_dmail_category', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_sysdmailcat.xlf');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_DirectMail', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_DirectMail.xlf');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_RecipientList', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_RecipientList.xlf');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_Statistics', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_Statistics.xlf');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_MailerEngine', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_MailerEngine.xlf');
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_DirectMailNavFrame_Configuration', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_Configuration.xlf');
+    //old
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('_MOD_web_DirectMailNavFrame', 'EXT:direct_mail/Resources/Private/Language/locallang_csh_web_txdirectmail.xlf');
 
     TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
         'DirectMailNavFrame',
@@ -99,7 +99,7 @@ if (TYPO3_MODE == 'BE') {
             ],
         ]
     );
-
+    
     TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
         'DirectMailNavFrame',
         'MailerEngine',
@@ -135,10 +135,10 @@ if (TYPO3_MODE == 'BE') {
             ],
         ]
     );
-}
 
-if (TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getExtensionVersion('tt_address')) <= TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('2.3.5')) {
-    include_once(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('direct_mail').'Configuration/TCA/Overrides/tt_address.php');
-}
-
-$GLOBALS['TBE_STYLES']['skins']['direct_mail']['stylesheetDirectories'][] = 'EXT:direct_mail/Resources/Public/StyleSheets/';
+    if (TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getExtensionVersion('tt_address')) <= TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger('2.3.5')) {
+        include_once(TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('direct_mail').'Configuration/TCA/Overrides/tt_address.php');
+    }
+    
+    $GLOBALS['TBE_STYLES']['skins']['direct_mail']['stylesheetDirectories'][] = 'EXT:direct_mail/Resources/Public/StyleSheets/';
+})();
