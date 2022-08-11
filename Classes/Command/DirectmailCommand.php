6efa6e49fe78b44aa8243e56327964a730375c9a
Igor Neyman <in@points.de>
2022-08-11 17:45:28 +0200

    add DirectmailCommand

---
+++ Classes/Command/DirectmailCommand.php
@@ -0,0 +1,43 @@
+<?php
+namespace DirectMailTeam\DirectMail\Command;
+
+use DirectMailTeam\DirectMail\Dmailer;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class DirectmailCommand extends Command
+{
+    /**
+     * Configure the command by defining the name, options and arguments
+     */
+    public function configure()
+    {
+        $this->setDescription('This command invokes dmailer in order to process queued messages.');
+        //$this->setHelp('');
+    }
+    
+    /**
+     * Executes the command for showing sys_log entries
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title($this->getDescription());
+        $io->warning('start');
+        /**
+         * The direct_mail engine
+         * @var $htmlmail Dmailer
+         */
+        $htmlmail = GeneralUtility::makeInstance(Dmailer::class);
+        $htmlmail->start();
+        $htmlmail->runcron();
+        $io->warning('end');
+        return Command::SUCCESS;
+    }
+}
