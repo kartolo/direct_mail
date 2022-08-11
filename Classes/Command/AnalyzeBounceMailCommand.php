d8c009b0b7ef15b26646281ee423b68e89e53d63
Igor Neyman <in@points.de>
2022-08-11 19:54:57 +0200

    update AnalyzeBounceMailCommand

--- Classes/Command/AnalyzeBounceMailCommand.php
+++ Classes/Command/AnalyzeBounceMailCommand.php
@@ -2,12 +2,15 @@
 namespace DirectMailTeam\DirectMail\Command;
 
 use DirectMailTeam\DirectMail\Dmailer;
+use DirectMailTeam\DirectMail\Readmail;
+use DirectMailTeam\DirectMail\Repository\SysDmailMaillogRepository;
 use Fetch\Server;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class AnalyzeBounceMailCommand extends Command
@@ -104,29 +107,140 @@ class AnalyzeBounceMailCommand extends Command
             }
         }
         if ($input->getOption('count')) {
-            $count = $input->getOption('count');
+            $count = (int)$input->getOption('count');
             //$io->writeln($count);
         }
+
+        // try connect to mail server
+        $mailServer = $this->connectMailServer($server, $port, $type, $user, $password, $io);
+        if ($mailServer instanceof Server) {
+            // we are connected to mail server
+            // get unread mails
+            $messages = $mailServer->search('UNSEEN', $count);
+            if(count($messages)) {
+                /** @var Message $message The message object */
+                foreach ($messages as $message) {
+                    // process the mail
+                    if ($this->processBounceMail($message)) {
+                        // set delete
+                        $message->delete();
+                    } else {
+                        $message->setFlag('SEEN');
+                    }
+                }
+            }
+            // expunge to delete permanently
+            $mailServer->expunge();
+            imap_close($mailServer->getImapStream());
+            return true;
+        }
+        else {
+            return Command::FAILURE;
+        }
+
+        return Command::SUCCESS;
+    }
+
+    /**
+     * Process the bounce mail
+     * @param Message $message the message object
+     * @return bool true if bounce mail can be parsed, else false
+     */
+    private function processBounceMail($message)
+    {
+        /** @var Readmail $readMail */
+        $readMail = GeneralUtility::makeInstance(Readmail::class);
         
+        // get attachment
+        $attachmentArray = $message->getAttachments();
+        $midArray = [];
+        if (is_array($attachmentArray)) {
+            // search in attachment
+            foreach ($attachmentArray as $attachment) {
+                $bouncedMail = $attachment->getData();
+                // Find mail id
+                $midArray = $readMail->find_XTypo3MID($bouncedMail);
+                if (false === empty($midArray)) {
+                    // if mid, rid and rtbl are found, then stop looping
+                    break;
+                }
+            }
+        } else {
+            // search in MessageBody (see rfc822-headers as Attachments placed )
+            $midArray = $readMail->find_XTypo3MID($message->getMessageBody());
+        }
+        
+        if (empty($midArray)) {
+            // no mid, rid and rtbl found - exit
+            return false;
+        }
+        
+        // Extract text content
+        $cp = $readMail->analyseReturnError($message->getMessageBody());
+        
+        $row = GeneralUtility::makeInstance(SysDmailMaillogRepository::class)->selectForAnalyzeBounceMail($midArray['rid'], $midArray['rtbl'], $midArray['mid']);
+        
+        // only write to log table, if we found a corresponding recipient record
+        if (!empty($row)) {
+            /** @var Connection $connection */
+            $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('sys_dmail_maillog');
+            try {
+                $midArray['email'] = $row['email'];
+                $insertFields = [
+                    'tstamp' => $this->getEXEC_TIME(),
+                    'response_type' => -127,
+                    'mid' => (int)$midArray['mid'],
+                    'rid' => (int)$midArray['rid'],
+                    'email' => $midArray['email'],
+                    'rtbl' => $midArray['rtbl'],
+                    'return_content' => serialize($cp),
+                    'return_code' => (int)$cp['reason']
+                ];
+                $connection->insert('sys_dmail_maillog', $insertFields);
+                $sql_insert_id = $connection->lastInsertId('sys_dmail_maillog');
+                return (bool)$sql_insert_id;
+            } catch (\Doctrine\DBAL\DBALException $e) {
+                // Log $e->getMessage();
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    
+    /**
+     * Create connection to mail server.
+     * Return mailServer object or false on error
+     *
+     * @param string $server
+     * @param int $port
+     * @param string $type
+     * @param string $user
+     * @param string $password
+     * @param SymfonyStyle $io
+     * @return bool|Server
+     */
+    private function connectMailServer(string $server, int $port, string $type, string $user, string $password, SymfonyStyle $io)
+    {
         // check if we can connect using the given data
         /** @var Server $mailServer */
         $mailServer = GeneralUtility::makeInstance(
-            \Fetch\Server::class,
+            Server::class,
             $server,
             $port,
             $type
         );
         
+        // set mail username and password
         $mailServer->setAuthentication($user, $password);
         
         try {
             $imapStream = $mailServer->getImapStream();
+            return $mailServer;
         } catch (\Exception $e) {
             $io->error('Connection using the given data is failed. Error message: '.$e->getMessage()); //scheduler.bounceMail.dataVerification
-            return Command::FAILURE;
+            return false;
         }
-        
-        $io->writeln('Test');
-        return Command::SUCCESS;
     }
 }
