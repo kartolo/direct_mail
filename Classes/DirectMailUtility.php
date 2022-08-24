f946f8ccc18aeacc0038193d8bb2cdd7cff22570
Igor Neyman <in@points.de>
2022-08-24 20:38:25 +0200

    remove old function 'fetchRecordsListValues'

--- Classes/DirectMailUtility.php
+++ Classes/DirectMailUtility.php
@@ -54,58 +54,6 @@ use TYPO3\CMS\Extbase\Object\ObjectManager;
  */
 class DirectMailUtility
 {
-
-    /**
-     * Get recipient DB record given on the ID
-     *
-     * @param array $listArr List of recipient IDs
-     * @param string $table Table name
-     * @param string $fields Field to be selected
-     *
-     * @return array recipients' data
-     */
-    public static function fetchRecordsListValues(array $listArr, $table, $fields = 'uid,name,email')
-    {
-        $outListArr = [];
-        if (is_array($listArr) && count($listArr)) {
-            $idlist = implode(',', $listArr);
-
-            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable($table);
-            $queryBuilder
-                ->getRestrictions()
-                ->removeAll()
-                ->add(GeneralUtility::makeInstance(DeletedRestriction::class));
-
-            $fieldArray = GeneralUtility::trimExplode(',', $fields);
-
-            // handle selecting multiple fields
-            foreach ($fieldArray as $i => $field) {
-                if ($i) {
-                    $queryBuilder->addSelect($field);
-                } else {
-                    $queryBuilder->select($field);
-                }
-            }
-
-            $res = $queryBuilder->from($table)
-                ->where(
-                    $queryBuilder->expr()->in(
-                        'uid',
-                        $queryBuilder->createNamedParameter(
-                            GeneralUtility::intExplode(',', $idlist),
-                            Connection::PARAM_INT_ARRAY
-                        )
-                    )
-                )
-                ->execute();
-
-            while ($row = $res->fetch()) {
-                $outListArr[$row['uid']] = $row;
-            }
-        }
-        return $outListArr;
-    }
-
     /**
      * Get the ID of page in a tree
      *
