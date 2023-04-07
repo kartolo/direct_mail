99f46037a9aee83f3a1e0b79d59f65e4771207a3
Igor Neyman <in@points.de>
2023-04-07 18:36:30 +0000

    refactor: change versions for v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,17 +7,15 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Ivan Kartolo',
     'author_email' => 'ivan.kartolo@dkd.de',
     'author_company' => 'd.k.d Internet Service GmbH',
-    'state' => 'stable',
+    'state' => 'alpha',
     'clearcacheonload' => 0,
-    'version' => '9.2.2',
+    'version' => '10.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.99.99',
-            'lowlevel' => '11.5.0-11.99.99',
-            'tt_address' => '5.3.0-7.0.99',
-            'php' => '7.4.0-8.1.99',
-            'jumpurl' => '8.0.3-',
-            'rdct' => '2.1.0',
+            'typo3' => '12.3.0-12.99.99',
+            'lowlevel' => '12.3.0-12.99.99',
+            'tt_address' => '7.1.0-7.1.99',
+            'php' => '8.1.0-8.2.99',
         ],
         'conflicts' => [
         ],
