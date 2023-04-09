7187c9e1dced2816bb35292ac356a3599bc0905e
Igor Neyman <in@points.de>
2023-04-09 12:28:43 +0000

    fix: update tca config for 'pages'

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -4,9 +4,9 @@ defined('TYPO3') || die();
 
 // pages modified
 $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-    'LLL:EXT:direct_mail/Resources/Private/Language/locallang_tca.xlf:pages.module.I.5',
-    'dmail',
-    'directmail-folder',
+    'label' => 'LLL:EXT:direct_mail/Resources/Private/Language/locallang_tca.xlf:pages.module.I.5',
+    'value' => 'dmail',
+    'icon' => 'directmail-folder',
 ];
 
 if (!is_array($GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'])) {
